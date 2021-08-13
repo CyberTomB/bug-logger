902888f5c0d49bc87166ef06b3601f3798f9ab43
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-13 15:36:26 -0600

    getbyid request added

--- bug-logger/server/controllers/BugsController.js
+++ bug-logger/server/controllers/BugsController.js
@@ -6,9 +6,29 @@ export class BugsController extends BaseController {
   constructor() {
     super('api/bugs')
     this.router
+      .get('', this.getAll)
+      .get('/:id', this.getOneById)
     // NOTE: Routes require auth
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const bugs = await bugsService.getAll()
+      res.send(bugs)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getOneById(req, res, next) {
+    try {
+      const bug = await bugsService.getById(req.params.id)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async create(req, res, next) {
--- bug-logger/server/services/BugsService.js
+++ bug-logger/server/services/BugsService.js
@@ -1,6 +1,14 @@
 import { dbContext } from '../db/DbContext'
 
 class BugsService {
+  async getAll(query = {}) {
+    return await dbContext.Bug.find(query).populate('creator')
+  }
+
+  async getById(id) {
+    return await dbContext.Bug.findById(id)
+  }
+
   async create(body) {
     const bug = await dbContext.Bug.create(body)
     return await dbContext.Bug.findById(bug._id).populate('creator')
