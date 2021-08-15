aad4bc2a7382613b871175ba7ac2b4c2139d122b
CyberDack <afntwrk.guyrpg@gmail.com>
2021-08-14 21:34:29 -0600

    bugs can toggle open only

--- bug-logger.client/checklist.md
+++ bug-logger.client/checklist.md
@@ -2,7 +2,7 @@
 * [X] At least 2 supported front end routes, Bugs and BugDetails
 * [X] The BugsPage shows all bugs (pagination allowed)
 * [] Bugs can be created
-* [] The Bugs can be sorted or filtered by their status
+* [X] The Bugs can be sorted or filtered by their status
 * [] Bugs display last modified date in a normalized format (MM-DD-YYYY)
 * [] Styling Indication on bugs page that bug is closed (color, strike-through, etc.)
 * [] Creating a new Bug automatically navigates to the BugDetails page
--- bug-logger.client/src/pages/BugsListPage.vue
+++ bug-logger.client/src/pages/BugsListPage.vue
@@ -11,22 +11,34 @@
         <h3 class="col-3">
           Last Updated
         </h3>
-        <h3 class="col">
-          Closed
+        <h3 class="col" @click="openFilter">
+          {{ state.openBugsOnly ? 'All Bugs' : 'Open Only' }}
         </h3>
       </div>
-      <BugListCard v-for="(bug, i) in bugs" :key="bug.id" :bug="bug" :index="i" />
+      <div v-if="state.openBugsOnly">
+        <BugListCard v-for="(bug, i) in openBugs"
+                     :key="bug.id"
+                     :bug="bug"
+                     :index="i"
+        />
+      </div>
+      <div v-else>
+        <BugListCard v-for="(bug, i) in bugs" :key="bug.id" :bug="bug" :index="i" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from '@vue/runtime-core'
+import { computed, onMounted, reactive } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import Pop from '../utils/Notifier'
 import { bugsService } from '../services/BugsService'
 export default {
   setup() {
+    const state = reactive({
+      openBugsOnly: false
+    })
     onMounted(async() => {
       try {
         await bugsService.getAll()
@@ -35,7 +47,12 @@ export default {
       }
     })
     return {
-      bugs: computed(() => AppState.bugs)
+      state,
+      bugs: computed(() => AppState.bugs),
+      openBugs: computed(() => AppState.bugs.filter(b => b.closed !== true)),
+      openFilter() {
+        state.openBugsOnly = !state.openBugsOnly
+      }
     }
   }
 }
